573907570f6de5a8d751936cf7e4c427e812cdec
Ubong Edem <ubongedem78@gmail.com>
2023-12-05 12:33:27 +0100

    working

--- gas_detector.ino
+++ gas_detector.ino
@@ -1,42 +1,46 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
 #include <SoftwareSerial.h>
 
-SoftwareSerial gsmSerial(6, 7); // SoftwareSerial for GSM module
-
-Adafruit_SSD1306 myDisplay(128, 64, &Wire); // OLED display
-
-String writeApiKey = "3DTMHW1H20UCVI7B"; // ThingSpeak API Key
-
+SoftwareSerial gsmSerial(6, 7);
+String writeApiKey = "3DTMHW1H20UCVI7B";
 const int gasThreshold = 75;
+#define I2C_ADDRESS 0x3C
+#define RST_PIN -1
 
+SSD1306AsciiWire oled;
+
+//------------------------------------------------------------------------------
 void setup() {
   Serial.begin(9600);
 
+  // Serial.println(F("Gas sensor warmed up!"));
+  Wire.begin();
+  Wire.setClock(400000L);
+
+#if RST_PIN >= 0
+  oled.begin(&Adafruit128x64, I2C_ADDRESS, RST_PIN);
+#else // RST_PIN >= 0
+  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+#endif // RST_PIN >= 0
+
+  oled.setFont(Adafruit5x7);
+
+  uint32_t m = micros();
+  oled.clear();
+  oled.set2X();
+  oled.println("Gas Detector Sys!");
+  oled.println();
+
   gsmSerial.begin(9600);
-  delay(2000);
-
-//  myDisplay.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-//  myDisplay.clearDisplay();
-//  myDisplay.setTextSize(2);
-//  myDisplay.setTextColor(SSD1306_WHITE);
-//  myDisplay.setCursor(0, 0);
-//  myDisplay.println(F("Gas Detector System"));
-//  myDisplay.display();
-
-  Serial.println(F("Initializing GSM..."));
-  delay(2000);
-
-  Serial.println(F("Warming up gas sensor..."));
-  delay(10000);
-  Serial.println(F("Gas sensor warmed up!"));
+  delay(14000);
 }
 
 void loop() {
   float sensorValue = analogRead(A0);
 
-//  updateDisplay(sensorValue);
+  updateDisplay(sensorValue);
 
   Serial.print(F("Gas Value: "));
   Serial.println(sensorValue);
@@ -45,22 +49,23 @@ void loop() {
     sendSMS("Gas detected! Take necessary action.");
     Serial.println("Sending to ThingSpeak...");
     sendToThingSpeak(sensorValue);
-    delay(5000);  // Wait before checking again
+    // Wait before checking again
+    delay(5000);
   }
 
   delay(2000);
 }
-//
-//void updateDisplay(float value) {
-//  myDisplay.clearDisplay();
-//  myDisplay.setTextSize(2);
-//  myDisplay.setTextColor(SSD1306_WHITE);
-//  myDisplay.setCursor(0, 0);
-//  myDisplay.print(F("Gas Value: "));
-//  myDisplay.print(value);
-//  myDisplay.display();
-//}
 
+// Update the OLED display with the given value.
+void updateDisplay(float value) {
+  oled.clear();
+  oled.set2X();
+  oled.println(F("Gas Value: "));
+  oled.println(value);
+  oled.println();
+}
+
+// Send data to ThingSpeak.
 void sendToThingSpeak(float gasValue) {
   Serial.println(F("Closing existing connections..."));
   gsmSerial.println(F("AT+CIPSHUT"));
@@ -119,6 +124,7 @@ void sendToThingSpeak(float gasValue) {
   delay(500);
 }
 
+// Send SMS
 void sendSMS(String message) {
   Serial.println(F("Sending SMS..."));
   gsmSerial.println(F("AT+CMGF=1")); // Set the GSM Module to text mode
@@ -127,9 +133,9 @@ void sendSMS(String message) {
   delay(100);
   gsmSerial.println(message);
   delay(100);
-  gsmSerial.write(26); // ASCII code of CTRL+Z
+  gsmSerial.write(26);
   delay(100);
   gsmSerial.println();
-  delay(5000); 
+  delay(5000);
   Serial.println(F("SMS sent."));
 }
