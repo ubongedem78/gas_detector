863a660fab0fd8d0bb42990b546d6c179184cfbe
Ubong Edem <ubongedem78@gmail.com>
2023-12-05 18:05:14 +0100

    latest_working_buzzer

--- gas_detector.ino
+++ gas_detector.ino
@@ -4,26 +4,23 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
 
-SoftwareSerial gsmSerial(6, 7); // SoftwareSerial for GSM module
-String writeApiKey = "3DTMHW1H20UCVI7B"; // ThingSpeak API Key
-const int gpsRxPin = 2; // Connect GPS TX to this pin on Arduino
-const int gpsTxPin = 3; // Connect GPS RX to this pin on Arduino
+SoftwareSerial gsmSerial(6, 7);
+String writeApiKey = "3DTMHW1H20UCVI7B";
+
+const int gpsRxPin = 2;
+const int gpsTxPin = 3;
+
 SoftwareSerial gpsSerial(gpsRxPin, gpsTxPin);
 const int gasThreshold = 75;
 TinyGPSPlus gps;
-// 0X3C+SA0 - 0x3C or 0x3D
 #define I2C_ADDRESS 0x3C
-
-// Define proper RST_PIN if required.
 #define RST_PIN -1
+#define BUZZER_PIN 8
 
 SSD1306AsciiWire oled;
-//------------------------------------------------------------------------------
+
 void setup() {
   Serial.begin(9600);
- 
-
- // Serial.println(F("Gas sensor warmed up!"));
   Wire.begin();
   Wire.setClock(400000L);
 
@@ -37,52 +34,52 @@ void setup() {
 
   uint32_t m = micros();
   oled.clear();
-   oled.set2X();
+  oled.set1X();
   oled.println("Gas Detection!");
-  //oled.println("A long line may be truncated");
   oled.println();
 
   gsmSerial.begin(9600);
   delay(14000);
   gpsSerial.begin(9600);
 }
-//------------------------------------------------------------------------------
+
 void loop() {
   float sensorValue = analogRead(A0);
 
   updateDisplay(sensorValue);
 
-//  Serial.print(F("Gas Value: "));
-//  Serial.println(sensorValue);
+  showGpsData();
 
-showGpsData();
-    
   String gpsMessage = "Latitude: " + String(gps.location.lat(), 6) +
-                            " | Longitude: " + String(gps.location.lng(), 6);
-
+                      ", Longitude: " + String(gps.location.lng(), 6);
 
   if (sensorValue > gasThreshold) {
-    
+    activateBuzzer();
     sendSMS("Gas detected! Take necessary action " + gpsMessage);
     Serial.println("Sending to ThingSpeak...");
     sendToThingSpeak(sensorValue, gps.location.lat(), gps.location.lng());
-    delay(5000);  // Wait before checking again
+    deactivateBuzzer();
+    delay(10000);
   }
 
   delay(2000);
 }
-//
+
 void updateDisplay(float value) {
-//  myDisplay.clearDisplay();
-//  myDisplay.setTextSize(2);
-oled.clear();
- oled.set2X();
-//  myDisplay.setTextColor(SSD1306_WHITE);
-//  myDisplay.setCursor(0, 0);
+  oled.clear();
+  oled.set1X();
+  //print gas value on oled
   oled.println(F("Gas Value: "));
-oled.println(value);
-oled.println();
-//  myDisplay.display();
+  oled.println(value);
+  oled.println();
+  if (gps.location.isValid()) {
+    oled.println(F("Latitude: "));
+    oled.println(gps.location.lat(), 6);
+    oled.println(F("Longitude: "));
+    oled.println(gps.location.lng(), 6);
+  } else {
+    oled.println(F("No GPS fix available"));
+  }
 }
 
 void sendToThingSpeak(float gasValue, float latitude, float longitude) {
@@ -147,39 +144,39 @@ void sendToThingSpeak(float gasValue, float latitude, float longitude) {
 
 void sendSMS(String message) {
   Serial.println(F("Sending SMS..."));
-  gsmSerial.println(F("AT+CMGF=1")); // Set the GSM Module to text mode
+  gsmSerial.println(F("AT+CMGF=1"));
   delay(2000);
   gsmSerial.println(F("AT+CMGS=\"+2347082257453\""));
   delay(100);
   gsmSerial.println(message);
   delay(100);
-  gsmSerial.write(26); // ASCII code of CTRL+Z
+  gsmSerial.write(26);
   delay(100);
   gsmSerial.println();
-  delay(5000); 
+  delay(5000);
   Serial.println(F("SMS sent."));
 }
 
 void showGpsData() {
   while (gpsSerial.available() > 0) {
-      
-    // Read the GPS data
     if (gps.encode(gpsSerial.read())) {
       Serial.println("I am here");
-      // Check if GPS data is valid
       if (gps.location.isValid()) {
-        // Print latitude and longitude
         Serial.print("Latitude: ");
         Serial.println(gps.location.lat(), 6);
         Serial.print(" | Longitude: ");
         Serial.println(gps.location.lng(), 6);
-//        String gpsMessage = "Latitude: " + String(gps.location.lat(), 6) +
-//                            " | Longitude: " + String(gps.location.lng(), 6);
-
       } else {
-        // If GPS data is not valid, print a message
         Serial.println("No GPS fix available");
       }
     }
   }
+}
+
+void activateBuzzer() {
+  tone(BUZZER_PIN, 1000);
+}
+
+void deactivateBuzzer() {
+  noTone(BUZZER_PIN);
 }
